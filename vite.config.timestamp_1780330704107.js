1b92da1681a12caeddf91322b5e4b4463132e815
Venkatesh Raju <venkatesh1754673niz@gmail.com>
2026-06-01 22:06:42 +0530

    fix: integrate nitro bundler for vercel serverless output

--- vite.config.timestamp_1780330704107.js
+++ vite.config.timestamp_1780330704107.js
@@ -1,18 +1,23 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
-
-export default defineConfig({
+var vite_config_default = defineConfig({
   plugins: [
     TanStackRouterVite(),
-    tanstackStart(),
-    nitro(),
+    TanStackStartVite({
+      server: {
+        preset: "vercel"
+      }
+    }),
     react(),
     tailwindcss(),
-    tsconfigPaths(),
-  ],
+    tsconfigPaths()
+  ]
 });
+export {
+  vite_config_default as default
+};
